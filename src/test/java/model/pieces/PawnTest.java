5e1795e1f99dd4711245f174955933e38c651ee0
Matt Stetter <mjs2617@rit.edu>
2021-11-13 15:29:42 -0500

    Mockito is working!!!!

--- src/test/java/model/pieces/PawnTest.java
+++ src/test/java/model/pieces/PawnTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import model.BoardState;
 import model.BoardState.Player;
+import model.ChessBoard;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * Test class for the Pawn piece
@@ -17,16 +20,10 @@ public class PawnTest {
   private Piece playerTwoPawn;
   private BoardState mockBoard;
 
-  @BeforeAll
-  public void setup() {
-    playerOnePawn = new Pawn(Player.ONE);
-    playerTwoPawn = new Pawn(Player.TWO);
-  }
-
   // PLAYER ONE TESTS
 
   @Test
   public void gameStartDoubleMove() {
-    
+    mockBoard = mock(ChessBoard.class);
   }
 }
