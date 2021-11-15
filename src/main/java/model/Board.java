932b7c6f8ca51b065adffad70f107ebc5555e014
Matt Stetter <mjs2617@rit.edu>
2021-11-14 21:54:58 -0500

    Stubbed out new methods in Board for running the chess game

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -7,4 +7,43 @@ package model;
  * @author Matt Stetter
  */
 public interface Board extends BoardState {
+
+  /**
+   * Tries to move the piece to the
+   * specified position. IllegalArgumentException
+   * is thrown if the move is invalid.
+   * @param p1 the starting position
+   * @param p2 the ending position
+   * @return INVALID if the move is invalid
+   *         MOVE if a move to an empty space
+   *         CAPTURE if the move is a capture move
+   *         CASTLE if the move is a castle
+   */
+  MoveType move(PiecePosition p1, PiecePosition p2) throws IllegalArgumentException;
+
+  enum MoveType {
+    INVALID,
+    MOVE,
+    CAPTURE,
+    CASTLE
+  }
+
+  /**
+   * Returns whether the game is over.
+   * A different enum value
+   * depending on the final state
+   * of the game.
+   * @return IN_PROGRESS while the game is going
+   *         P1_WINNER if player 1 wins
+   *         P2_WINNER if player 2 wins
+   *         STALEMATE if the game is a stalemate
+   */
+  FinalState isGameOver();
+
+  enum FinalState {
+    IN_PROGRESS,
+    P1_WINNER,
+    P2_WINNER,
+    STALEMATE
+  }
 }
--- src/main/java/model/ChessBoard.java
+++ src/main/java/model/ChessBoard.java
@@ -1,6 +1,12 @@
 package model;
 
+import model.pieces.Bishop;
+import model.pieces.King;
+import model.pieces.Knight;
+import model.pieces.Pawn;
 import model.pieces.Piece;
+import model.pieces.Queen;
+import model.pieces.Rook;
 
 /**
  * Simple class for a chess board.
@@ -10,12 +16,15 @@ import model.pieces.Piece;
 public class ChessBoard implements Board {
   private Piece[][] board;
   private Player curPlayer;
+  private final int height, width;
 
   /**
    * Generates a chess board.
    * (8x8 grid with traditional setup)
    */
   public ChessBoard() {
+    this.height = 8;
+    this.width = 8;
     this.initializeBoard();
     curPlayer = Player.ONE;
   }
@@ -23,28 +32,69 @@ public class ChessBoard implements Board {
   // Initializes the board with the classic arrangement
   // of the pieces on the board.
   private void initializeBoard() {
-
+    this.board = new Piece[height][width];
+    int rowStart = 0;
+    int rowIter = 1;
+    Player p = Player.ONE;
+    for (int i = 0; i < 2; i++) {
+      this.board[rowStart][0] = new Rook(p);
+      this.board[rowStart][1] = new Knight(p);
+      this.board[rowStart][2] = new Bishop(p);
+      this.board[rowStart][3] = new Queen(p);
+      this.board[rowStart][4] = new King(p);
+      this.board[rowStart][5] = new Bishop(p);
+      this.board[rowStart][6] = new Knight(p);
+      this.board[rowStart][7] = new Rook(p);
+      rowStart += rowIter;
+      for (int j = 0; j < width; j++) {
+        this.board[rowStart][j] = new Pawn(p);
+      }
+      rowStart = 7;
+      rowIter = -1;
+    }
   }
 
   @Override
   public int getBoardWidth() {
-    return 0;
+    return this.width;
   }
 
   @Override
   public int getBoardHeight() {
-    return 0;
+    return this.height;
   }
 
   // Returns the piece at the provided position.
   @Override
-  public Piece getPieceAt(PiecePosition pos) {
-    return null;
+  public Piece getPieceAt(PiecePosition pos) throws IllegalArgumentException {
+    if ((pos.getRow() < 0 || pos.getRow() >= this.height)
+      || (pos.getColumn() < 0 || pos.getColumn() >= this.width)) {
+      throw new IllegalArgumentException(String.format("Invalid position: (%d, %d)%n",
+              pos.getRow(), pos.getColumn()));
+    }
+    return this.board[pos.getRow()][pos.getColumn()];
   }
 
   // Returns the player that is currently playing.
   @Override
   public Player getCurrentPlayer() {
     return this.curPlayer;
+  }
+
+  // Tries to make the provided move,
+  // throws IllegalArgumentException
+  // if the move is invalid. Returns
+  // the type of move that was made.
+  public MoveType move(PiecePosition p1, PiecePosition p2) {
+
+
+    return MoveType.MOVE;
+  }
+
+  // Returns the state of the game
+  // as an enum.
+  public FinalState isGameOver() {
+
+    return FinalState.P1_WINNER;
   }
 }
